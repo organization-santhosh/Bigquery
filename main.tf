15ae70a8751548c5017d083f2744da42d8dd0ad4
santhosh <santhoshsai80@gmail.com>
2022-09-19 22:24:33 +0530

    update

--- main.tf
+++ main.tf
@@ -1,32 +1,28 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.36.0"
-    }
+resource "google_bigquery_dataset" "dataset" {
+    dataset_id = "mydata"
+  
+}
+
+resource "google_bigquery_table" "table" {
+    table_id = "testtable"
+    dataset_id = google_bigquery_dataset.dataset.dataset_id
+    schema =  <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
   }
+]
+EOF
+
+
+  
 }
-
-provider "google" {
-  # Configuration options
-  project = "terraform-practice2"
-  region = "us-central1"
-  zone = "us-central1-a"
-}
-
-
-resource "google_cloud_run_service" "test" {
-  name = "cloud-run1"
-    location = "asia-southeast1"
-    #project = "terraform-practice2"
-    template {
-      spec {
-        containers {
-          image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
-      }
-      }
-
-
-
-      }
